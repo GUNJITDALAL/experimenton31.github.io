f5a3e8e1f37a696b9daffc404b5fc5e270a88d16
gunjit <73329409+GUNJITDALAL@users.noreply.github.com>
2021-01-31 20:39:45 +0530

    Add files via upload

---
+++ ifelse.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    int a = 100;
+    if(a<20){
+        printf("number isgreater a=%d");
+    }
+    else{
+        printf("number is less a=%d");
+    }
+    printf("\nthe number a is=%d",a);
+    return 0;
+}
